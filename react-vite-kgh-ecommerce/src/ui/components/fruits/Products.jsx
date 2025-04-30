2381bff48d7cbe776773c00bd3304989400ff677
9hakboiii <gyuhakarmy@gmail.com>
2025-04-30 15:39:20 +0900

    이미지 처리

--- react-vite-kgh-ecommerce/src/ui/components/fruits/Products.jsx
+++ react-vite-kgh-ecommerce/src/ui/components/fruits/Products.jsx
@@ -96,7 +96,7 @@ const Products = () => {
                       {filterProuct.map((product) => (
                         <div key={products.id} className="col-md-6 col-lg-4 col-xl-3">
                           <div className="rounded position-relative fruite-item">
-                            <div className="fruite-img">
+                            <div className="fruite-img ratio ratio-4x3 overflow-hidden rounded-top ">
                               <img
                                 src={`${import.meta.env.VITE_REQUEST_URL}${product.image}`}
                                 className="img-fluid w-100 rounded-top"
@@ -114,8 +114,8 @@ const Products = () => {
                               <p>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt
                               </p>
-                              <div className="d-flex justify-content-between flex-lg-wrap">
-                                <p className="text-dark fs-5 fw-bold mb-0">${product.price} / kg</p>
+                              <div className="d-flex flex-column align-items-center justify-content-between flex-lg-wrap">
+                                <p className="text-dark fs-5 fw-bold mb-2">${product.price} / kg</p>
                                 <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary">
                                   <i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart
                                 </a>
