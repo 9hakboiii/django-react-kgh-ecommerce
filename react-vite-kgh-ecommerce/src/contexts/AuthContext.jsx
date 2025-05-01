535fc1933001219b137c4a0790d72cf319f74986
9hakboiii <gyuhakarmy@gmail.com>
2025-05-01 16:11:32 +0900

    JWT (4)

---
+++ react-vite-kgh-ecommerce/src/contexts/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from 'react';
+import { loginUser } from '@/ui/api/AuthApi';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const login = async (username, password) => {
+    try {
+      const response = await loginUser(username, password);
+    } catch (error) {
+      console.error('로그인 실패', error);
+      throw error;
+    }
+  };
+};
--- react-vite-kgh-ecommerce/src/router/Router.jsx
+++ react-vite-kgh-ecommerce/src/router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../ui/layouts/MainLayout';
 import Products from '@/ui/components/fruits/products';
+import Login from '@/ui/components/login/Login';
 
 const routes = [
   {
@@ -11,6 +12,12 @@ const routes = [
       {
         path: '',
         element: <Products></Products>,
+        loader: () => '상품들',
+      },
+      {
+        // dev_5_fruit
+        path: 'login',
+        element: <Login></Login>,
         loader: () => '상품들',
       },
     ],
