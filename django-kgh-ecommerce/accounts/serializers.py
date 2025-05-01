8572e671893cec69eca556ece132ee3d92307f3b
9hakboiii <gyuhakarmy@gmail.com>
2025-05-01 14:40:49 +0900

    JWT (3)

---
+++ django-kgh-ecommerce/accounts/serializers.py
@@ -0,0 +1,32 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
+from .models import User
+
+# dev_5_fruit
+# djsoer 회원가입 커스텀 마이징
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "gender",
+            "job",
+            # "old_cart",
+        )
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "gender",
+            "job",
+            "old_cart",
+            "create_at",
+            "update_at",
+        )
--- django-kgh-ecommerce/config/settings.py
+++ django-kgh-ecommerce/config/settings.py
@@ -259,3 +259,15 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+# djoser 커스텀마이징 세팅
+DJOSER = {
+    "USER_ID_FIELD": "id",
+    "LOGIN_FIELD": "username",  # 또는 email
+    "SERIALIZERS": {
+        "user_create": "accounts.serializers.UserCreateSerializer",
+        "user": "accounts.serializers.UserSerializer",
+        "current_user": "accounts.serializers.UserSerializer",
+    },
+    "CREATE_SESSION_ON_LOGIN": True,  # 로그인하면 세션도 생성됨
+}
