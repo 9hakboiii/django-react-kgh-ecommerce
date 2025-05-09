c2b1c63d3cbcf5f79f04eb0a8229c6bb5aea5e53
9hakboiii <gyuhakarmy@gmail.com>
2025-05-09 09:38:24 +0900

    로그아웃 cart 삭제 처리

--- react-vite-kgh-ecommerce/src/contexts/AuthContext.jsx
+++ react-vite-kgh-ecommerce/src/contexts/AuthContext.jsx
@@ -52,6 +52,9 @@ export const AuthProvider = ({ children }) => {
     setAccessToken(null)
     localStorage.removeItem('access')
     localStorage.removeItem('refresh')
+
+    // dev_7_fruit
+    localStorage.removeItem('cart')
   }
 
   const value = {
